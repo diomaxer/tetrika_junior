ed2ca65fa8088d45ce3f97145554252081470c62
diomaxer <68566883+diomaxer@users.noreply.github.com>
2022-07-17 17:54:33 +0300

    task 3

--- tetrika_task3.py
+++ tetrika_task3.py
@@ -1,89 +1,74 @@
-'''
-'pupil': [2033, 6347],
-'tutor': [2017, 2066, 2068, 6341]},
+def append_interval(intervals, name):
+	mini_array = []
 
-'''
+	for i in range(len(intervals[name])):
+		if i % 2 == 0:
+			mini_array.append((intervals[name][i], name[0], 'join'))
+		else:
+			mini_array.append((intervals[name][i], name[0], 'out'))
 
-def find_intervals(int1, int2):
-    int1_start = [int1[elem] for elem in range(len(int1)) if elem % 2 == 0]
-    int1_end = [int1[elem] for elem in range(len(int1)) if elem % 2 != 0]
-    int2_start = [int2[elem] for elem in range(len(int2)) if elem % 2 == 0]
-    int2_end = [int2[elem] for elem in range(len(int2)) if elem % 2 != 0]
-
-    print(int1_start)
-    print(int1_end)
-    print(int2_start)
-    print(int2_end)
-
-
-'''
- {'data': {'lesson': [2000, 5600],
-              'pupil': [2033, 6347],
-              'tutor': [2017, 2066, 2068, 6341]},
-     'answer': 3565
-     },
-'''
-
-
+	return mini_array
 
 def appearance(intervals):
-    # tutor = intervals['tutor']
+	array = []
+	array += append_interval(intervals, 'lesson')
+	array += append_interval(intervals, 'pupil')
+	array += append_interval(intervals, 'tutor')
 
-    tutor_start = [1, 2, 6, 14, 17]
-    tutor_end = [4, 5, 12, 22, 35]
-    start = end = 0
+	array.sort(key=lambda x : x[0])
+
+	tracker = {
+		'l': 0,
+		'p': 0,
+		't': 0
+	}
+
+	start = ans = 0
+
+	for elem in array:
+
+		if elem[2] == 'join':
+			tracker[elem[1]] += 1
+		elif elem[2] == 'out':
+			tracker[elem[1]] -= 1
+
+		# print(tracker)
 
 
-    array = {tutor_start[0], }
-    j = 0
-    for i in range(1, len(tutor_end)):
-        while j < len(tutor_end) and tutor_start[j] < tutor_end[i]:     # 4 6
-            j += 1
+		if not 0 in (tracker.values()):
+			start = elem[0]
 
-        if j >= len(tutor_start):
-            break
+		if 0 in (tracker.values()) and start != 0:
+			# print(elem[0], '-', start)
+			ans += elem[0] - start
+			start = 0
 
-        start = tutor_start[j]
-        end = tutor_end[i]
-
-        print(tutor_start[j], tutor_end[i])
-
-        if tutor_start[j] > tutor_end[i] and len(array) % 2 == 0:
-            array.add(tutor_start[j])
-        if tutor_start[j] <= tutor_end[i + 1] and len(array) % 2 == 1:   #
-            array.add(tutor_end[i])
-
-        print(array)
-
-    print(array)
+	return ans
 
 
 
 
 tests = [
-    # {'data': {'lesson': [1594663200, 1594666800],
-    #          'pupil': [1594663340, 1594663389, 1594663390, 1594663395, 1594663396, 1594666472],
-    #          'tutor': [1594663290, 1594663430, 1594663443, 1594666473]},
-    #  'answer': 3117
-    # },
-    # {'data': {'lesson': [1594702800, 1594706400],
-    #          'pupil': [1594702789, 1594704500, 1594702807, 1594704542, 1594704512, 1594704513, 1594704564, 1594705150, 1594704581, 1594704582, 1594704734, 1594705009, 1594705095, 1594705096, 1594705106, 1594706480, 1594705158, 1594705773, 1594705849, 1594706480, 1594706500, 1594706875, 1594706502, 1594706503, 1594706524, 1594706524, 1594706579, 1594706641],
-    #          'tutor': [1594700035, 1594700364, 1594702749, 1594705148, 1594705149, 1594706463]},
-    # 'answer': 3577
-    # },
-    # {'data': {'lesson': [1594692000, 1594695600],
-    #          'pupil': [1594692033, 1594696347],
-    #          'tutor': [1594692017, 1594692066, 1594692068, 1594696341]},
-    # 'answer': 3565
-    # },
-    {'data': {'lesson': [2000, 5600],
-              'pupil': [2033, 6347],
-              'tutor': [2017, 2066, 2068, 6341]},
-     'answer': 3565
-     },
+    {'data': {'lesson': [1594663200, 1594666800],
+             'pupil': [1594663340, 1594663389, 1594663390, 1594663395, 1594663396, 1594666472],
+             'tutor': [1594663290, 1594663430, 1594663443, 1594666473]},
+     'answer': 3117
+    },
+    {'data': {'lesson': [1594702800, 1594706400],
+             'pupil': [1594702789, 1594704500, 1594702807, 1594704542, 1594704512, 1594704513, 1594704564, 1594705150, 1594704581, 1594704582, 1594704734, 1594705009, 1594705095, 1594705096, 1594705106, 1594706480, 1594705158, 1594705773, 1594705849, 1594706480, 1594706500, 1594706875, 1594706502, 1594706503, 1594706524, 1594706524, 1594706579, 1594706641],
+             'tutor': [1594700035, 1594700364, 1594702749, 1594705148, 1594705147, 1594706463]},
+    'answer': 3577
+    },
+    {'data': {'lesson': [1594692000, 1594695600],
+             'pupil': [1594692033, 1594696347],
+             'tutor': [1594692017, 1594692066, 1594692068, 1594696341]},
+    'answer': 3565
+    },
 ]
 
+
+
 if __name__ == '__main__':
-   for i, test in enumerate(tests):
+    for i, test in enumerate(tests):
        test_answer = appearance(test['data'])
-#        assert test_answer == test['answer'], f'Error on test case {i}, got {test_answer}, expected {test["answer"]}'
+       assert test_answer == test['answer'], f'Error on test case {i}, got {test_answer}, expected {test["answer"]}'
